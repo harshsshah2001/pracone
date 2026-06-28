61751ce20c96d7346982aa6e1a43c46dfdf219b7
harshsshah2001 <Graphionicinfotech-1801>
2026-06-28 20:43:43 +0530

    harsh

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,25 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use AdminService as GlobalAdminService;
 use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Mail;
+use App\Services\admin\AdminService;
 
 class AdminController extends Controller
 {
+    protected $adminService;
 
-    public function showAdminHome(Request $request){
+    public function __construct(AdminService $adminService)
+    {
+        $this->adminService = $adminService;
+    }
+
+
+
+
+    public function showAdminHome(Request $request)
+    {
         return response()->json([
-            'status'=>TRUE,
-            'message'=>'Admin Home',
-            'user'=>$request->user(),
-            'amount'=>1000
+            'status' => TRUE,
+            'message' => 'Admin Home',
+            'user' => $request->user(),
+            'amount' => 1000
         ]);
     }
 
-    public function addCategories(){
-
-    }
+    public function addCategories() {}
 }
---
+++ app/Services/admin/AdminService.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use App\Models\Payment;
+use Illuminate\Http\Request;
+use Razorpay\Api\Api;
+use App\Models\Register;
+use App\Models\Registration_Images;
+use App\Repository\RegisterRepository;
+use Illuminate\Support\Facades\Hash;
+
+class AdminService{
+    
+}
